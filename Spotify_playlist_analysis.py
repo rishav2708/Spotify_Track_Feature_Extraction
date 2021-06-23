789509e1754f01f8ea770e605bd6e3c8ea607f60
Rishav <rishav2708@users.noreply.github.com>
2021-06-23 23:37:50 +0530

    Update Spotify_playlist_analysis.py

--- Spotify_playlist_analysis.py
+++ Spotify_playlist_analysis.py
@@ -3,10 +3,12 @@ from csv import DictWriter
 
 from spotipy.oauth2 import SpotifyOAuth
 
-#scope = "user-library-read"
-scope = "user-read-currently-playing"
+scope_user_lib = "user-library-read"
+scope_user_current_read = "user-read-currently-playing"
+scope_user_recently_played = "user-read-recently-played"
+scope_user_follow_read ="user-follow-read"
 SPOTIPY_CLIENT_ID = "800343d83a574916a368c4d9a7910859"
-SPOTIPY_CLIENT_SECRET = "1dabe407bf0d45a4b85dcef282ca53f5"
+SPOTIPY_CLIENT_SECRET = "c6c4482bee4843828a4b768aeb1ddec0"
 SPOTIPY_REDIRECT_URI = "http://localhost:1419"
 os.environ['SPOTIPY_CLIENT_ID'] = SPOTIPY_CLIENT_ID
 os.environ['SPOTIPY_CLIENT_SECRET'] = SPOTIPY_CLIENT_SECRET
@@ -33,6 +35,15 @@ def get_saved_lib_tracks(scope):
 	for i in range(10):
 		print (results['items'][i]['track']['name'], (results['items'][i]['track']['id']))
 
+def get_audio_analysis_tracks(spObj, track_name, track_id):
+	results = spObj.audio_analysis(track_id)
+	print (results.keys())
+	print("beats")
+	print (results["beats"])
+
+	
+	
+
 def get_audio_features_tracks(spObj,track_name, track_id):
 	results = spObj.audio_features(track_id)
 	#print (list(results[0].keys()))
@@ -42,16 +53,49 @@ def get_audio_features_tracks(spObj,track_name, track_id):
 	track_contents["name"] = track_name
 	writeToCSV(track_features,track_contents)
 
+def get_user_following(scope):
+	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+	#results_users = sp.current_user_followed_users(limit=20)
+	results_artists = sp.current_user_followed_artists(limit=20)
+	#print (results_users)
+	artist_lists = results_artists["artists"]["items"]
+	for i in range(len(artist_lists)):
+		print (artist_lists[i]["name"])
+		print (artist_lists[i]["popularity"])
+		print (artist_lists[i]["genres"])
+def get_recently_played_tracks(scope):
+	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+	results = sp.current_user_recently_played(limit=50)
+	track_iter = []
+	#print (results.keys())
+	#print (results["items"][10].keys())
+	#print (results["items"][10])
+	for i in results["items"]:
+		#print(i["played_at"], i["track"]["name"], i["track"]["id"])
+		track_iter.append(i["track"]["name"])
+		#print ("Context:"+i["context"]+" Played At:"+i["played_at"]+" Name:"+i["track"]["name"])
+	#print (results["cursors"])
+	print (track_iter)
 def get_current_playing_track_info(scope):
 	sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 	results = sp.current_user_playing_track()
 	track_name = results['item']['name']
 	track_id = results['item']['id']
 	get_audio_features_tracks(sp, track_name, track_id)
+	#get_audio_analysis_tracks(sp,track_name,track_id)
 
 def main():
-	get_current_playing_track_info(scope)
+	starter = sys.argv[1]
+	print (starter)
 
+	if(starter =="currently_playing"):
+		get_current_playing_track_info(scope_user_current_read)
+	elif (starter =="recently_played"):
+		get_recently_played_tracks(scope_user_recently_played)
+	elif (starter =="following"):
+		get_user_following(scope_user_follow_read)
+	else:
+		print ("pass valid param")
 
 if __name__ == "__main__":
 	main()
